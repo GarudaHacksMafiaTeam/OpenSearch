e4b1e19b878630cbece5392da44cd6a3589584e6
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-20 21:07:16 +0700

    edited landing page

--- src/components/index/Landing.tsx
+++ src/components/index/Landing.tsx
@@ -2,53 +2,113 @@ import React from 'react'
 import style from 'styles/layout/landing.module.css'
 import Image from 'next/image'
 import LandingNavbar from 'components/layout/LandingNavbar'
+import Link from 'next/link'
 import Head from 'next/head'
+import Footer from 'components/layout/Footer'
 
 const Headline = () => {
 	return (
-		<section className={style.headline}>
+		<header className={style.content}>
 			<div className={style.left}>
-				<div className={style.desc}>
+				<div className={style.textbox}>
 					<h1 className={style.title}>
 						Create, Search, and Manage your <span className={style.red}>Open Source Project</span>
 					</h1>
-					<p className={style.subtitle}>
-					Join our worldwide open source community
+					<p className={style.desc}>
+						Join our worldwide open source community
 					</p>
 				</div>
-				<button className={style.button}>Join Now</button>
+				<Link href='/auth/signin'>
+					<a>
+						<button className={style.button}>
+							Join Now
+						</button>
+					</a>
+				</Link>
 			</div>
 			<div className={style.right}>
-				<Image src="/assets/landing/landing-img.svg" alt="landing" width="803" height="619"/>
+				<Image src="/assets/landing/landing-img-1.svg" alt="landing-img-1" width="803" height="619"/>
 			</div>
-		</section>
+		</header>
 	)
 }
 
 const About = () => {
 	return (
-		<section className={style.about}>
+		<section id="about" className={style.content}>
 			<div className={style.left}>
-
+				<Image className={style.leftimg} src="/assets/landing/landing-img-2.svg" alt="landing-img-2" width="828" height="750"/>
 			</div>
 			<div className={style.right}>
-				<h1 className={style.title}>
-					A Collaboration Platform
-				</h1>
-				<p className={style.subtitle}>
-					Join, create, and market your open source platform at OpenSearch! You can also join to search and contribute to existing open source!
-				</p>
+				<div className={style.textbox}>
+					<p className={style.aboutblock}>
+						About Us
+					</p>
+					<h1 className={ [style.title2, style.red].join(' ') }>
+						A Collaboration Platform
+					</h1>
+					<p className={style.desc}>
+						Join, create, and market your open source platform at OpenSearch! You can also join to search and contribute to existing open source!
+					</p>
+				</div>
 			</div>
 		</section>
 	)
 }
 
+const ActionTile = ({children, ...props}) => {
+	return (
+		<div className={style.tilecontainer}>
+			<div className={style.tileblock}>
+				<div className={style.tilebox}>
+					<div className={style.blockimg}>
+						{ children }
+					</div>
+					<p>
+						{ props.step }
+					</p>
+				</div>
+				<p>
+					{ props.desc }
+				</p>
+			</div>
+		</div>
+	)
+}
+
 const Action = () => {
 	return (
-		<section className={style.action}>
-			
-			<h1>How it Works?</h1>
-			<p>Simple steps to follow on using our platform</p>
+		<section id="action" className={[style.content, style.bgnavy].join(' ')}>
+			<div>
+				<div className={style.actiontitle}>
+					<h1>
+						How it Works?
+					</h1>
+					<p>
+						Simple steps to follow on using our platform
+					</p>
+				</div>
+				<div className={style.tilecontainer}>
+					<ActionTile
+						step="Register yourself"
+						desc="Fill in your email, the registration is passwordless!"
+					>
+						<Image src="/assets/landing/landing-img-3.svg" alt="yes" width="250" height="300"/>
+					</ActionTile>
+					<ActionTile
+						step="Create and share your Open Source Project"
+						desc="Create feeds & customize your open source profile!"
+					>
+						<Image src="/assets/landing/landing-img-4.svg" alt="yes" width="250" height="300"/>
+					</ActionTile>
+					<ActionTile
+						step="Join Open Source"
+						desc="Explore your favorite open source projects and join them!"
+					>
+						<Image src="/assets/landing/landing-img-5.svg" alt="yes" width="250" height="300"/>
+					</ActionTile>
+				</div>
+			</div>
 		</section>
 	)
 }
@@ -66,6 +126,7 @@ const Landing = () => {
 			<Headline />
 			<About />
 			<Action />
+			<Footer />
 		</div>
 	)
 }
---
+++ src/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from 'styles/layout/footer.module.css'
+
+const Footer = () => {
+	return (
+		<footer className={style.footer}>
+			Copyright OpenSearch 2021
+		</footer>
+	)
+}
+
+export default Footer
