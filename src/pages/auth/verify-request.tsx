60c46f1045ad9781b37bcf92c6d3d98aca5dcfef
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-11-20 17:19:14 +0700

    feature(verify-request): add verify request page

---
+++ src/pages/auth/verify-request.tsx
@@ -0,0 +1,18 @@
+import styles from "styles/auth/verify-request/index.module.css"
+import Image from 'next/image'
+
+export default function VerifyRequest({ csrfToken }) {
+  return (
+    <div className={styles.verifyRequestPage}>
+      <div className={styles.leftPart}>
+        <Image src="/assets/verify-request/verify-request.svg" alt="me" width="480" height="625" />
+      </div>
+      <div className={styles.rightPart}>
+        <h1 className={styles.title}>Thanks for your registration</h1>
+        <p className={styles.subtitle}>
+        We have sent a verification message to your email address. Please check your inbox
+        </p>
+      </div>
+    </div>
+  );
+}
