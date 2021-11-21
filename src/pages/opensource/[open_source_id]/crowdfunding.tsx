0ce04d5d1bee95390e8adfa249538687532f6cf0
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-21 20:47:49 +0700

    removed several donation history from crowdfunding

--- src/pages/opensource/[open_source_id]/crowdfunding.tsx
+++ src/pages/opensource/[open_source_id]/crowdfunding.tsx
@@ -8,35 +8,19 @@ import Masonry from 'react-masonry-css'
 const OpenSourceProfile = () => {
   const donationHistory = [
     {
-      donator: "malik",
-      desc: "keren gan, semangat terus yaw xoxo",
+      donator: "Malik",
+      desc: "What a great open source! hope it will get big soon enough",
     },
     {
-      donator: "azhar",
-      desc: "guaassssssssssssssssssssss ssssssssss ssssssssssssss sssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssss gannnnnnn nnnnnnnnnnnnnn nnnnnnnnnnnnnn nnnnnnnnnnn",
-    },
-    {
-      donator: "mazaya",
-      desc: "yoyoyooyoyoyoyooyooyoooooooooooo mantapppp sekali man tidak apa apa  autoprefixer: end value has mixed support, consider using flex-end instead",
-    },
-    {
-      donator: "fia",
-      desc: "ini cuma warning: autoprefixer: end value has mixed support, consider using flex-end instead",
-    },
-    {
-      donator: "malik",
-      desc: "keren gan, semangat terus yaw xoxo",
-    },
-    {
-      donator: "malik",
-      desc: "keren gan, semangat terus yaw xoxo",
+      donator: "Azhar",
+      desc: "You guys did such a great job!",
     },
   ];
-  
+
   return (
     <>
       <div className={styles.crowdfundContainer}>
-        <Masonry 
+        <Masonry
           breakpointCols={2}
           className={styles.myMasonryGrid}
           columnClassName={styles.myMasonryGridColumn}
@@ -49,7 +33,7 @@ const OpenSourceProfile = () => {
             {
               donationHistory.map((donation) => {
                 return (
-                  <FundBox {...donation}></FundBox>
+                  <FundBox {...donation} />
                 );
               })
             }
