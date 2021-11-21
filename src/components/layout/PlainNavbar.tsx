e57a8ae51f2177ceeef02cc3006eaa56ae4f7772
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-21 22:30:03 +0700

    add hamburger bar for side bar

--- src/components/layout/PlainNavbar.tsx
+++ src/components/layout/PlainNavbar.tsx
@@ -5,19 +5,24 @@ import { BiBell } from 'react-icons/bi'
 import Link from 'next/link'
 import { useState } from 'react'
 import PopUpNotif from 'components/layout/PopUpNotif'
-import Sidebar from './SideBar'
 import { useSession } from 'next-auth/client'
 import SignOutPopUp from './SignOutPopUp'
+import Hamburger from 'hamburger-react'
+import Sidebar from './SideBar'
 
 // Kalau bisa page cuman representatif dari struktur page dan ngga pake api client 
 const PlainNavbar = () => {
   const [openNotif, setOpenNotif] = useState(false);
   const [openSignOutOpt, setSignOutOpt] = useState(false);
-  const [session, loading] = useSession()
+  const [session, loading] = useSession();
+  const [showSideBar, setShowSideBar] = useState(false);
 
   const handleClick = () => {
     setOpenNotif(!openNotif);
     console.log('openNotif', openNotif);
+  }
+  const handlePopUp = () => {
+    console.log("clicked");
   }
 
   return (
@@ -26,37 +31,42 @@ const PlainNavbar = () => {
         !loading
         &&
         <>
-          <div className={styles.popup}>
-            {/* 
-            <div className={styles.sidebarContainer}>
+          {
+            showSideBar &&
+            <div className={styles.sideBarContainer}>
               <Sidebar />
+              <div className={styles.overlay} onClick={() => setShowSideBar(false)}></div>
             </div>
-            */}
-            {/* <div className="overlay"></div> */}
-          </div>
+          }
           <div className={styles.base}>
-            <Link href="/">
-              <a >
-                <h1 className={styles.brand}>
-                  Open<strong>Search</strong>
-                </h1>
-              </a>
-            </Link>
-            <div className={styles.rightnav}>
-              <div className={styles.notifContainer}>
-                <BiBell color='white' size='1.5rem' className={styles.bell} onClick={handleClick} />
-                {
-                  openNotif && <PopUpNotif />
-                }
+            <div className={styles.popup} onClick={() => setShowSideBar(true)}>
+              <Hamburger toggled={showSideBar} rounded />
+            </div>
+            <div className={styles.navbarContainer}>
+              <div>
+                <Link href="/">
+                  <a >
+                    <h1 className={styles.brand}>
+                      Open<strong>Search</strong>
+                    </h1>
+                  </a>
+                </Link>
               </div>
-              <div className={styles.notifContainer}>
-                <Avatar round={true} name="Mikasa Ackerman" size="1.5rem" onClick={() => setSignOutOpt(!openSignOutOpt)} className={styles.avatar} />
-                {
-                  openSignOutOpt && <SignOutPopUp />
-                }
+              <div className={styles.rightnav}>
+                <div className={styles.notifContainer}>
+                  <BiBell color='white' size='1.5rem' className={styles.bell} onClick={handleClick} />
+                  {
+                    openNotif && <PopUpNotif />
+                  }
+                </div>
+                <div className={styles.notifContainer}>
+                  <Avatar round={true} name="Mikasa Ackerman" size="1.5rem" onClick={() => setSignOutOpt(!openSignOutOpt)} className={styles.avatar} />
+                  {
+                    openSignOutOpt && <SignOutPopUp />
+                  }
+                </div>
+                <h1 className={styles.name}>{session.user.name ? session.user.name : "Anonymous"}</h1>
               </div>
-              <h1 className={styles.name}>{session.user.name ? session.user.name : "Anonymous"}</h1>
-
             </div>
           </div>
         </>
